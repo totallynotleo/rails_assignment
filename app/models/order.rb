7304e9efd1bda1cf6c607008c2d5447dac2f206b
totallynotleo <leodabrim@gmail.com>
2019-05-05 14:03:11 +1000

    I can't remember exactly what i did because i meant to make this commit 2 days ago, mostly bug fixes getting listing_id to work in orders and adding a footer and header.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :listing_orders
-  has_many :listings, through: :listing_orders
+  has_many :listings, :through => :listing_orders
 end
